74c8c4830212997e4d560d435de42f9aa1f6bc8b
morachake <jacobmoracha@gmail.com>
2025-01-02 02:13:57 +0300

    Feat:adding initial app flow

---
+++ app/(auth)/splash.tsx
@@ -0,0 +1,12 @@
+import { View ,Text} from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+export default function Splash() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Splash</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
